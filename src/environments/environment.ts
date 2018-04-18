c9dd5277c47cb4c6c1b393b703c9184e11b1773e
vlad.s.dobrescu <vlad.s.dobrescu@gmail.com>
2018-04-18 23:39:01 +0300

    firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,12 @@
 
 export const environment = {
     production: false,
-    firebase: [
-        
-    ]
+    firebase: {
+        apiKey: "AIzaSyDLKPFlwFETlVBHgje0uAhpKIqcXORKXc0",
+        authDomain: "fubardigital.firebaseapp.com",
+        databaseURL: "https://fubardigital.firebaseio.com",
+        projectId: "fubardigital",
+        storageBucket: "fubardigital.appspot.com",
+        messagingSenderId: "637121743562"
+    }
 };
