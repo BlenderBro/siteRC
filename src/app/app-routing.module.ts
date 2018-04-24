f3128f1ade50410a7f3e04ec03ad766333290703
vlad.s.dobrescu <vlad.s.dobrescu@gmail.com>
2018-04-24 19:27:22 +0300

    estimator page scafolding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+// Components
 import { SocialComponent } from './partials/social/social.component';
-
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { LoginComponent } from './partials/login/login.component';
+import { EstimatorComponent } from './pages/estimator/estimator.component';
+
+// Services
 import { IsAuthenticatedService } from './core/is-authenticated.service';
+
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'contact', component: ContactComponent, canActivate: [IsAuthenticatedService] },
     { path: 'lemein', component: LoginComponent },
+    { path: 'cost-estimation', component: EstimatorComponent },
 ];
 
 @NgModule({
@@ -25,5 +29,6 @@ export const RoutedComponents = [
     ContactComponent,
     SocialComponent,
     LoginComponent,
+    EstimatorComponent,
 ]
     
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { IsAuthenticatedService } from './core/is-authenticated.service';
 
 
 
+
 @NgModule({
     declarations: [
         AppComponent,
