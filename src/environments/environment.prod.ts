407a44f3f2cead35242309fd84b949a32040cd1a
vlad.s.dobrescu <vlad.s.dobrescu@gmail.com>
2018-05-04 13:03:48 +0300

    about page created

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyDLKPFlwFETlVBHgje0uAhpKIqcXORKXc0",
+        authDomain: "fubardigital.firebaseapp.com",
+        databaseURL: "https://fubardigital.firebaseio.com",
+        projectId: "fubardigital",
+        storageBucket: "fubardigital.appspot.com",
+        messagingSenderId: "637121743562"
+    }
 };
